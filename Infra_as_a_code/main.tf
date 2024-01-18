ecc0318be912da03d412f3b4848449a274dd58c0
antznette1 <0chiezeanthonette@gmail.com>
2024-01-18 22:17:45 +0000

    Update CI/CD configuration and Terraform state files

--- Infra_as_a_code/main.tf
+++ Infra_as_a_code/main.tf
@@ -1,9 +1,9 @@
-resource "aws_lambda_function" "myfunc1" {
+resource "aws_lambda_function" "myfunc" {
   filename         = data.archive_file.zip_the_python_code.output_path
   source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
-  function_name    = "myfunc1"
+  function_name    = "myfunc"
   role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "func.handler"
+  handler          = "func.lambda_handler" 
   runtime          = "python3.12"
 }
 
@@ -28,34 +28,37 @@ EOF
 }
 
 resource "aws_iam_policy" "iam_policy_for_resume_project" {
-
   name        = "aws_iam_policy_for_terraform_resume_project_policy"
   path        = "/"
   description = "AWS IAM Policy for managing the resume project role"
-    policy = jsonencode(
+
+  policy = jsonencode(
     {
-      "Version" : "2012-10-17",
-      "Statement" : [
+      "Version": "2012-10-17",
+      "Statement": [
         {
-          "Action" : [
+          "Effect": "Allow",
+          "Action": [
             "logs:CreateLogGroup",
             "logs:CreateLogStream",
             "logs:PutLogEvents"
           ],
-          "Resource" : "arn:aws:logs:*:*:*",
-          "Effect" : "Allow"
+          "Resource": "arn:aws:logs:*:*:*"
         },
         {
-          "Effect" : "Allow",
-          "Action" : [
+          "Effect": "Allow",
+          "Action": [
             "dynamodb:UpdateItem",
-			      "dynamodb:GetItem"
+            "dynamodb:GetItem",
+            "dynamodb:PutItem"  // Add this line to allow PutItem
           ],
-          "Resource" : "arn:aws:dynamodb:*:*:table/tonette_resume"
+          "Resource": "arn:aws:dynamodb:*:*:table/tonette_resume"
         },
       ]
-  })
+    }
+  )
 }
+
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
   role = aws_iam_role.iam_for_lambda.name
@@ -70,7 +73,7 @@ data "archive_file" "zip_the_python_code" {
 }
 
 resource "aws_lambda_function_url" "url1" {
-  function_name      = aws_lambda_function.myfunc1.function_name
+  function_name      = aws_lambda_function.myfunc.function_name
   authorization_type = "NONE"
 
   cors {
